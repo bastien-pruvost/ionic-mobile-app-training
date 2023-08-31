0140f97395af6844968fee818405c5bf775e23fb
Bastien Pruvost <bastien.mory@gmail.com>
2023-08-31 13:53:24 +0200

    feat: init ios and android

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>My Ionic App</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,6 +16,7 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <p>Hello world</p>
         <ExploreContainer />
       </IonContent>
     </IonPage>
